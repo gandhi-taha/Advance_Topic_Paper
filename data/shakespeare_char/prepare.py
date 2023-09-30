8e7a651991f4cd042cfaa2fa9a471ab8087e732b
Taha Gandhi <33763107+gandhi-taha@users.noreply.github.com>
2023-09-30 21:23:13 +0200

    Add files via upload

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -1,68 +1,61 @@
-"""
-Prepare the Shakespeare dataset for character-level language modeling.
-So instead of encoding with GPT-2 BPE tokens, we just map characters to ints.
-Will save train.bin, val.bin containing the ids, and meta.pkl containing the
-encoder and decoder and some other related info.
-"""
 import os
 import pickle
 import requests
 import numpy as np
 
-# download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+# Define the URL for the Shakespeare dataset
+data_url = 'https://raw.githubusercontent.com/gandhi-taha/Advance_Topic_Paper/main/min_shakespeare.txt'
+
+# Download the dataset if it doesn't exist
+input_file_path = 'input.txt'
 if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/gandhi-taha/Advance_Topic_Paper/main/min_shakespeare.txt'
     with open(input_file_path, 'w') as f:
         f.write(requests.get(data_url).text)
 
+# Read the dataset from the file
 with open(input_file_path, 'r') as f:
     data = f.read()
-print(f"length of dataset in characters: {len(data):,}")
 
-# get all the unique characters that occur in this text
+print(f"Length of dataset in characters: {len(data):,}")
+
+# Get all unique characters in the text
 chars = sorted(list(set(data)))
 vocab_size = len(chars)
-print("all the unique characters:", ''.join(chars))
-print(f"vocab size: {vocab_size:,}")
+print("All the unique characters:", ''.join(chars))
+print(f"Vocabulary size: {vocab_size:,}")
 
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
+# Create a mapping from characters to integers
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+
 def encode(s):
-    return [stoi[c] for c in s] # encoder: take a string, output a list of integers
+    return [stoi[c] for c in s]
+
 def decode(l):
-    return ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+    return ''.join([itos[i] for i in l])
 
-# create the train and test splits
+# Create train and validation splits
 n = len(data)
-train_data = data[:int(n*0.9)]
-val_data = data[int(n*0.9):]
+train_data = data[:int(n * 0.9)]
+val_data = data[int(n * 0.9):]
 
-# encode both to integers
+# Encode both splits to integers
 train_ids = encode(train_data)
 val_ids = encode(val_data)
-print(f"train has {len(train_ids):,} tokens")
-print(f"val has {len(val_ids):,} tokens")
+print(f"Train has {len(train_ids):,} tokens")
+print(f"Validation has {len(val_ids):,} tokens")
 
-# export to bin files
+# Export to binary files
 train_ids = np.array(train_ids, dtype=np.uint16)
 val_ids = np.array(val_ids, dtype=np.uint16)
-train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+train_ids.tofile('train.bin')
+val_ids.tofile('val.bin')
 
-# save the meta information as well, to help us encode/decode later
+# Save meta information to help with encoding/decoding later
 meta = {
     'vocab_size': vocab_size,
     'itos': itos,
     'stoi': stoi,
 }
-with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
+with open('meta.pkl', 'wb') as f:
     pickle.dump(meta, f)
-
-# length of dataset in characters:  1115394
-# all the unique characters:
-#  !$&',-.3:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
-# vocab size: 65
-# train has 1003854 tokens
-# val has 111540 tokens
